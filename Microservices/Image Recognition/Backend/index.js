2b8151fd314f821ce9b6dc07cfc3f8f24e574fbe
Aidan Tilgner <aidantilgner02@gmail.com>
2022-05-28 01:10:06 -0700

    finished backend implementation
    Signed-off-by: Aidan Tilgner <aidantilgner02@gmail.com>

--- Microservices/Image Recognition/Backend/index.js
+++ Microservices/Image Recognition/Backend/index.js
@@ -2,19 +2,53 @@ import * as tf from "@tensorflow/tfjs-node";
 import coco_ssd from "@tensorflow-models/coco-ssd";
 import express from "express";
 import { config } from "dotenv";
+// busboy import
+import busboy from "busboy";
 config();
 
-const App = express();
-const PORT = process.env.PORT || 5000;
-
-console.log(tf.getBackend());
-let model;
+// * INIT MODEL
+let model = undefined;
 (async () => {
   model = await coco_ssd.load({
-    base: "lite_mobilenet_v2",
+    base: "mobilenet_v1",
   });
 })();
 
-// App.listen(PORT, () => {
-//   console.log(`Server started on port ${PORT}`);
-// });
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
+
+const wrapAsync = (fn) => {
+  return (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+};
+
+app.post("/predict", (req, res) => {
+  if (!model) {
+    res.status(500).send("Model is not loaded yet!");
+    return;
+  }
+
+  console.log("Hit /predict endpoint");
+  const bb = busboy({ headers: req.headers });
+  console.log("busboy created");
+  bb.on("file", (fieldname, file, filename, encoding, mimetype) => {
+    console.log("Type: ", mimetype);
+    const buffer = [];
+    file.on("data", (data) => {
+      buffer.push(data);
+    });
+    file.on("end", async () => {
+      const image = tf.node.decodeImage(Buffer.concat(buffer));
+      const predictions = await model.detect(image, 3, 0.25);
+      console.log("Predictions: ", predictions);
+      res.json(predictions);
+    });
+  });
+  req.pipe(bb);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
