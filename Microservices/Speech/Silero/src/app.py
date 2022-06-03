85b52e7651543f0bbadcb5dbd31355712475e745
Aidan Tilgner <aidantilgner02@gmail.com>
2022-06-02 22:27:55 -0700

    added silero backend server
    Signed-off-by: Aidan Tilgner <aidantilgner02@gmail.com>

---
+++ Microservices/Speech/Silero/src/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template, redirect, url_for
+from sst import *
+import os
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+
+# The stt post request will take an audio file and return predicted text
+@app.route("/stt", methods=["POST"])
+def sst():
+    if request.method == "POST":
+        file = request.files["file"]
+        ext = file.filename.split(".")[-1]
+        filename = "temp." + ext
+        file.save(os.path.join(os.path.dirname(__file__), "tmp/" + filename))
+        text = predictAudio(os.path.join(os.path.dirname(__file__), "tmp/" + filename))
+        os.remove(os.path.join(os.path.dirname(__file__), "tmp/" + filename))
+        return {"text": text}
+
+
+# start server on port 5000
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ Microservices/Speech/Silero/src/sst.py
@@ -0,0 +1,42 @@
+import onnx
+import torch
+import onnxruntime
+from omegaconf import OmegaConf
+import os
+
+# import everything from utils in ./utils/utils.py
+# from utils.utils import *
+
+# TODO: Maybe make this call the web version after all?
+language = "en"
+print("Loading model", os.path.join(os.path.dirname(__file__), "utils/"))
+decoder, utils = torch.hub._load_local(
+    hubconf_dir=os.path.join(os.path.dirname(__file__), "utils/"),
+    model="silero_stt",
+    language=language,
+)
+(read_batch, split_into_batches, read_audio, prepare_model_input) = utils
+
+onnx_model = onnx.load(os.path.join(os.path.dirname(__file__), "models/model.onnx"))
+onnx.checker.check_model(onnx_model)
+ort_session = onnxruntime.InferenceSession(
+    os.path.join(os.path.dirname(__file__), "models/model.onnx")
+)
+
+
+def predictAudio(audio_path):
+    """
+    audio_path: path to audio file
+    """
+    # download a single file, any format compatible with TorchAudio
+    print("Reading audio", audio_path)
+    test_files = [audio_path]
+    batches = split_into_batches(test_files, batch_size=10)
+    input = prepare_model_input(read_batch(batches[0]))
+
+    # actual onnx inference and decoding
+    onnx_input = input.detach().cpu().numpy()
+    ort_inputs = {"input": onnx_input}
+    ort_outs = ort_session.run(None, ort_inputs)
+    decoded = decoder(torch.Tensor(ort_outs[0])[0])
+    return decoded
