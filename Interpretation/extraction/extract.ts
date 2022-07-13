82897ac9a2948dcb3bfc66a0f14ad5af36ec32c5
Aidan Tilgner <aidantilgner02@gmail.com>
2022-07-12 23:13:03 -0700

    normalizing music
    Signed-off-by: Aidan Tilgner <aidantilgner02@gmail.com>

--- Interpretation/extraction/extract.ts
+++ Interpretation/extraction/extract.ts
@@ -2,5 +2,5 @@ import { Extraction } from "./index.d";
 import { getInputMapping } from "../mappings/index";
 
 export const runNLU = (input: string) => {
-  return getInputMapping(input);
+  return getInputMapping(input.toLowerCase());
 };
