49c09871270f0a6c7019702fa6208c500b8e89f7
Aidan Tilgner <aidantilgner02@gmail.com>
2022-07-24 13:48:24 -0700

    added weather route to third parties api
    Signed-off-by: Aidan Tilgner <aidantilgner02@gmail.com>

--- Third Parties/index.ts
+++ Third Parties/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import path from "path";
 import { Request, Response } from "./index.d";
+import { config } from "dotenv";
 import weatherRouter from "./routes/weather";
 const app = express();
+
+config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- Third Parties/routes/weather.ts
+++ Third Parties/routes/weather.ts
@@ -1,13 +1,41 @@
 import { Router } from "express";
 import axios from "axios";
+import { config } from "dotenv";
 const router = Router();
+
+config();
+
+const locations = {
+  home: {
+    coords: {
+      lat: "44.86",
+      lon: "-123.01",
+    },
+    location: {
+      city: "Salem",
+      state: "OR",
+      country: "US",
+    },
+  },
+};
 
 router.get("/", async (req, res) => {
   try {
-    const { data } = await axios.get(
-      "https://api.openweathermap.org/data/2.5/weather?q=London&appid=b6907d289e10d714a6e88b30761fae22"
-    );
-    res.send(data);
+    const env = process.env.API_KEY_OPEN_WEATHER;
+    let lat, lon;
+    if (req.query.lon && req.query.lat) {
+      lat = req.query.lat;
+      lon = req.query.lon;
+    } else {
+      lat = locations.home.coords.lat;
+      lon = locations.home.coords.lon;
+    }
+    const url = req.query.location
+      ? `https://api.openweathermap.org/data/2.5/weather?q=${req.query.location}&appid=${env}`
+      : `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${env}`;
+
+    const { data } = await axios.get(url);
+    res.send({ data, message: "Successfully retrieved weather data" });
   } catch (err) {
     console.log("Error fetching weather data", err);
     return {
