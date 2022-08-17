4b157d9180ca1ed28453f931a5eb0869a91c2925
Aidan Tilgner <aidantilgner02@gmail.com>
2022-08-17 01:08:46 -0700

    added edit button functionality to intents and actions

---
+++ Interpretation/public/scripts/home.js
@@ -0,0 +1,174 @@
+import { setAlert } from "./display.js";
+const TestingInput = document.getElementById("testing-input");
+const TestingSubmit = document.getElementById("testing-submit");
+const TestingOutput = document.getElementById("testing-output");
+const TestingAction = document.getElementById("testing-action");
+const TestingIntent = document.getElementById("testing-intent");
+const TestingResponse = document.getElementById("testing-response");
+
+const state = {};
+
+const checkDisplayProperties = () => {
+  const OutputProperties = document.querySelectorAll(".output-property");
+  OutputProperties.forEach((property) => {
+    if (!state[property.dataset.state]) {
+      property.style.display = "none";
+      return;
+    }
+    property.style.display = "flex";
+  });
+};
+checkDisplayProperties();
+
+const checkDisplayOutput = () => {
+  if (TestingOutput.innerHTML.length > 0) {
+    TestingOutput.style.display = "flex";
+  } else {
+    TestingOutput.style.display = "none";
+  }
+};
+checkDisplayOutput();
+
+const clickHandler = async () => {
+  const { nlu } = await axios
+    .post("/nlu", {
+      text: TestingInput.value,
+    })
+    .then((res) => {
+      console.log("Response:", res.data);
+      return res.data;
+    })
+    .catch((err) => {
+      console.log("Error:", err);
+      setAlert(
+        "Error getting NLU response, check console for more info.",
+        "danger"
+      );
+    });
+  state.intent = nlu.intent;
+  state.nlu_response = nlu.nlu_response;
+  state.action = nlu.action;
+  TestingOutput.innerHTML = nlu.nlu_response;
+  TestingIntent.innerHTML = `<span><strong>Intent</strong>: ${nlu.intent}</span>`;
+  TestingAction.innerHTML = `<span><strong>Action</strong>: ${nlu.action}</span>`;
+  TestingResponse.innerHTML = `<span><strong>Response</strong>: ${nlu.nlu_response}</span>`;
+  checkDisplayOutput();
+  checkDisplayProperties();
+  addPropertyEventListeners();
+  return nlu;
+};
+
+TestingSubmit.addEventListener("click", clickHandler);
+// Testing Input should also call clickHandler when enter is pressed.
+TestingInput.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    clickHandler();
+  }
+});
+
+const createEditInputGroup = ({
+  placeholder = "Enter text",
+  value = "",
+  buttons,
+}) => {
+  const EditInputGroup = document.createElement("div");
+  EditInputGroup.classList.add("output-edit-property");
+  const input = document.createElement("input");
+  input.value = value;
+  input.setAttribute("type", "text");
+  input.setAttribute("placeholder", placeholder);
+  input.classList.add("output-edit-property__input");
+  EditInputGroup.appendChild(input);
+  const icons = document.createElement("div");
+  icons.classList.add("output-edit-property__icons");
+  buttons.forEach((button) => {
+    const icon = document.createElement("i");
+    icon.classList.add("material-symbols-outlined");
+    icon.classList.add(button.type);
+    icon.title = button.title;
+    icon.innerHTML = button.icon;
+    icon.onclick = button.action;
+    icons.appendChild(icon);
+  });
+  EditInputGroup.appendChild(icons);
+
+  return {
+    EditInputGroup,
+    input,
+    icons,
+  };
+};
+
+const handleEditIntentClick = (e) => {
+  e.preventDefault();
+  const { EditInputGroup, input, icons } = createEditInputGroup({
+    placeholder: "Enter the correct intent",
+    value: state.intent,
+    buttons: [
+      {
+        title: "Cancel",
+        icon: "cancel",
+        type: "danger",
+        action: () => {
+          console.log("Cancel clicked");
+          EditInputGroup.remove();
+          TestingIntentContainer.style.display = "flex";
+        },
+      },
+      {
+        title: "Submit new intent",
+        icon: "check",
+        type: "primary",
+        action: () => {},
+      },
+    ],
+  });
+  const OutputProperties = document.getElementById("testing-properties");
+  const TestingIntentContainer = document.getElementById(
+    "testing-intent-container"
+  );
+  // Set display none on testing-intent and make EditInputGroup the first child of testing-properties.
+  TestingIntentContainer.style.display = "none";
+  OutputProperties.insertBefore(EditInputGroup, TestingIntentContainer);
+};
+
+const handleEditActionClick = (e) => {
+  e.preventDefault();
+  const TestingActionContainer = document.getElementById(
+    "testing-action-container"
+  );
+  const { EditInputGroup, input, icons } = createEditInputGroup({
+    placeholder: "Enter the correct action for this intent",
+    value: state.action,
+    buttons: [
+      {
+        title: "Cancel",
+        icon: "cancel",
+        type: "danger",
+        action: () => {
+          console.log("Cancel clicked");
+          EditInputGroup.remove();
+          TestingActionContainer.style.display = "flex";
+        },
+      },
+      {
+        title: "Submit new action",
+        icon: "check",
+        type: "primary",
+        action: () => {},
+      },
+    ],
+  });
+  const OutputProperties = document.getElementById("testing-properties");
+  // Set display none on testing-intent and make EditInputGroup the first child of testing-properties.
+  TestingActionContainer.style.display = "none";
+  OutputProperties.insertBefore(EditInputGroup, TestingActionContainer);
+};
+
+const addPropertyEventListeners = () => {
+  const EditIntent = document.getElementById("edit-intent");
+  EditIntent.onclick = handleEditIntentClick;
+
+  const EditAction = document.getElementById("edit-action");
+  EditAction.onclick = handleEditActionClick;
+};
