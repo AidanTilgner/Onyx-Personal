21b8b06c461d545632845c5d36279d34e29693ea
Aidan Tilgner <aidantilgner02@gmail.com>
2022-08-30 22:28:46 -0700

    fixed typo in auth middleware for app server

--- Applications/routes/auth.ts
+++ Applications/routes/auth.ts
@@ -8,13 +8,14 @@ const router = Router();
 router.post("/check", (req, res) => {
   console.log("Body", req.body);
   const token = req.body.token;
+  console.log("Process Token: ", process.env.APP_KEY);
   if (!token) {
     return res.send({
       message: "You are currently unauthorized to use Onyx Dashboard.",
       authorized: false,
     });
   }
-  if (token !== process.env.API_KEY) {
+  if (token !== process.env.APP_KEY) {
     return res.send({
       message: "You are currently unauthorized to use Onyx Dashboard.",
       authorized: false,
