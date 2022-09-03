493c37b6ae98e215ee6454abe05beb53b0e60c28
Aidan Tilgner <aidantilgner02@gmail.com>
2022-09-03 00:11:48 -0700

    added proxy for packages

--- Applications/utils/logger.ts
+++ Applications/utils/logger.ts
@@ -1,6 +1,10 @@
 import { emitMessage } from "./socket-io";
 
-export const globalLog = (prepend: string, out: any, ...args: any[]) => {
-  console.log(JSON.stringify(out));
-  emitMessage("console_message", `${prepend}: ${JSON.stringify(out)}`, ...args);
+export const globalLog = (prepend: string, out?: any, ...args: any[]) => {
+  console.log(`${prepend}: ${JSON.stringify(out) || ""}`, ...args);
+  emitMessage(
+    "console_message",
+    `${prepend}: ${JSON.stringify(out) || ""}`,
+    ...args
+  );
 };
